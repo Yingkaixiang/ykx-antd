c6120c461a1754d545ca0e9141aec46a2999604f
应开翔 <ykx@qiniu-yingkaixiang.local>
2018-07-19 18:04:14 +0800

    feat: 完成简单的 Menu 功能

--- src/app/components/menu/index.tsx
+++ src/app/components/menu/index.tsx
@@ -1,15 +1,58 @@
+import classNames from "classnames";
 import * as React from "react";
 import { Link } from "react-router-dom";
 import "./index.less";
 
-export default class Menu extends React.Component {
+interface IMenu {
+  routes?: any[];
+}
+
+export default class Menu extends React.Component<IMenu, any> {
+  constructor(props: IMenu) {
+    super(props);
+    this.state = {
+      routes: this.props.routes,
+    };
+  }
+
+  navigateTo = (i: number): void => {
+    const { routes } = this.state;
+    const list = routes.map((item: any, index: number) => {
+      console.log(i);
+      console.log(index);
+      if (index === i) {
+        item.isSelected = true;
+      } else {
+        item.isSelected = false;
+      }
+      return item;
+    });
+    console.dir(list);
+    this.setState({ routes: list });
+  };
+
+  renderRoutes = () => {
+    const { routes } = this.state;
+    return routes.map((item: any, index: number) => {
+      const menuItemCls = classNames({
+        "ykx-menu-item": true,
+        "ykx-menu-item-selected": item.isSelected,
+      });
+      return (
+        <li
+          className={menuItemCls}
+          key={index}
+          onClick={this.navigateTo.bind(this, index)}
+        >
+          <Link to={item.url}>
+            {item.title} <span className="ykx-chinese">{item.titleChs}</span>
+          </Link>
+        </li>
+      );
+    });
+  };
+
   render() {
-    return (
-      <div className="ykx-menu">
-        <Link to="/">Home</Link>
-        <Link to="/markdown">markdown</Link>
-        <Link to="/badge">badge</Link>
-      </div>
-    );
+    return <ul className="ykx-menu">{this.renderRoutes()}</ul>;
   }
 }
--- src/app/routes/markdown/index.tsx
+++ src/app/routes/markdown/index.tsx
@@ -7,17 +7,22 @@ const markdownText = `# Markdown 编辑器
 **粗体**
 `;
 
+const markdownContent =
+  "# Markdown 编辑器\n<p>可以使用 ```Markdown``` 语法进行编写。</p>\n## 何时使用\n<p>当你需要编写文档时可以使用当前组件。</p>";
+
 function MarkdownDoc() {
   return (
-    <Codebox
-      title="基础"
-      desc="可以在编辑器中使用所有的 ```Markdown``` 语法"
-      style={{ margin: 20 }}
-      language="markdown"
-      code={markdownText}
-    >
-      <Markdown source={markdownText} />
-    </Codebox>
+    <div style={{ padding: 64 }}>
+      <Markdown source={markdownContent} />
+      <Codebox
+        title="基础"
+        desc="可以在编辑器中使用所有的 ```Markdown``` 语法"
+        language="markdown"
+        code={markdownText}
+      >
+        <Markdown source={markdownText} />
+      </Codebox>
+    </div>
   );
 }
 
